5153116d3a0a57702814863d2d4173cf718820d2
Gustavo <gustavo.biscaro@gmail.com>
2022-09-28 20:57:23 -0300

    Script adicionado!

---
+++ js/scripts.js
@@ -0,0 +1,6 @@
+﻿const candidatos = [
+    {id: 01, nome:'Felipo Lima', numeroEleitoral: 22},
+    {id: 02, nome:'Guto, simplesmente', numeroEleitoral: 17},
+    {id: 03, nome:'Isaac do Truco', numeroEleitoral: 42},
+    {id: 04, nome:'Guigui Adams', numeroEleitoral: 69}
+];

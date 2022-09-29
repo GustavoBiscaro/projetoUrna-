3edfeb663f9c6999d4943e7a7cda4792d2f779aa
Gustavo Miguel <gustavo.biscaro@gmail.com>
2022-09-29 13:15:45 -0300

    Script Changes

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,9 @@
 ﻿
 //Candidatos
 const candidatos = [
-    {id: 01, nome:'Felipo Lima', numeroEleitoral: 22},
-    {id: 02, nome:'Guto, simplesmente', numeroEleitoral: 17},
-    {id: 03, nome:'Isaac do Truco', numeroEleitoral: 42},
-    {id: 04, nome:'Guigui Adams', numeroEleitoral: 69}
-    {id: 04, nome:'Gustavin', numeroEleitoral: 7}
+    {id: 01, nome:'Felipo Lima', numeroEleitoral: 22, partido: 'PT(Partido Dos Trabalhadores)'},
+    {id: 02, nome:'Guto, simplesmente', numeroEleitoral: 17, partido: 'Novo'},
+    {id: 03, nome:'Isaac do Truco', numeroEleitoral: 42, partido: 'Frente Nacionalista'},
+    {id: 04, nome:'Guigui Adams', numeroEleitoral: 69, partido: 'PCO'}
+    {id: 04, nome:'Gustavin', numeroEleitoral: 7, partido: 'Mestre Do Capitalismo'}
 ];

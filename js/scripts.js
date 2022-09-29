95c1042981d21525bb463cb7bcfa24ac86da3cfa
Gustavo Miguel <gustavo.biscaro@gmail.com>
2022-09-29 12:11:00 -0300

    Introduction Javascript

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,9 @@
-﻿const candidatos = [
+﻿
+//Candidatos
+const candidatos = [
     {id: 01, nome:'Felipo Lima', numeroEleitoral: 22},
     {id: 02, nome:'Guto, simplesmente', numeroEleitoral: 17},
     {id: 03, nome:'Isaac do Truco', numeroEleitoral: 42},
     {id: 04, nome:'Guigui Adams', numeroEleitoral: 69}
+    {id: 04, nome:'Gustavin', numeroEleitoral: 7}
 ];
